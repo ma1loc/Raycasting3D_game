624919cdb1d130cef304572fa902234c70af9102
youness anflous <anayouness51@gmail.com>
2025-07-04 14:48:33 +0100

    fix the line drowing logic

--- game_randring/img_randring.c
+++ game_randring/img_randring.c
@@ -34,26 +34,6 @@ void	draw_square(int x, int y, int size, int color)
 	}
 }
 
-void	draw_line(t_game *game, int x, int y, int line_size)
-{
-	int		i;
-	int		j;
-
-	(void)y;
-	(void)x;
-	i = 0;
-	while (i < line_size)
-	{
-		j = 0;
-		while (j < line_size)
-		{
-			my_mlx_pixel_put(game, x, y + j, GREEN_CLOOR);
-			j++;
-		}
-		i++;
-	}
-}
-
 void	draw_2d_map(t_game *game)
 {
 	int	y;
@@ -76,18 +56,35 @@ void	draw_2d_map(t_game *game)
 	}	
 }
 
-void	draw_player(t_game *game)
+void	draw_line(t_game *game, int line_size)
 {
-	int	player_screen_x;
-	int player_screen_y;
-	int	player_size;
-	int	line_size;
+    int     i;
+    double  dx;
+    double  dy;
+    double  px;
+    double  py;
+    int     draw_x;
+    int     draw_y;
 
-	line_size = 60;
-	player_size = TILE_SIZE / 3;
-	player_screen_x = (int)game->player.pos.p_x - (player_size / 2);
-	player_screen_y = (int)game->player.pos.p_y - (player_size / 2);
-	
-	draw_square(player_screen_x, player_screen_y, player_size, RED_COLOR);
-	draw_line(game, player_screen_x, player_screen_y, line_size);
+    dx = cos(game->player.angle);
+    dy = sin(game->player.angle);
+    px = game->player.pos.p_x;
+    py = game->player.pos.p_y;
+    for (i = 0; i < line_size; i++)
+    {
+        draw_x = (int)(px + dx * i);
+        draw_y = (int)(py + dy * i);
+        my_mlx_pixel_put(game, draw_x, draw_y, GREEN_CLOOR);
+    }
+}
+
+void draw_player(t_game *game)
+{
+    int player_size = TILE_SIZE / 3;
+    int player_screen_x = (int)(game->player.pos.p_x - player_size / 2);
+    int player_screen_y = (int)(game->player.pos.p_y - player_size / 2);
+    int line_size = 30;
+
+    draw_line(game, line_size);
+    draw_square(player_screen_x, player_screen_y, player_size, RED_COLOR);
 }

2881930b0edb82739a766bb47f35c3868a552f6e
youness anflous <anayouness51@gmail.com>
2025-07-10 20:26:18 +0100

    just a comments

--- comments.c
+++ comments.c
@@ -96,196 +96,102 @@
 
 // ---------------------------------------------------------------------------
 
-double	get_distance(t_coord p_pos, t_coord hit)
-{
-	double	dx;
-	double	dy;
+// #define EPSILON 0.001
 
-	// no matter of the p_pos.x or the hit.x orderd
-	dx = p_pos.x - hit.x;
-	dy = p_pos.y - hit.y;
-	return (sqrt((dx * dx) + (dy * dy)));
-}
+// t_intercept_hit get_horizontal_intersection(t_game *game, t_coord p_pos, double ray_angle)
+// {
+//     double          slope;
+//     t_intercept_hit intercept;
+//     t_coord         step;
 
+//     while (ray_angle < 0) ray_angle += 2 * M_PI;
+//     while (ray_angle >= 2 * M_PI) ray_angle -= 2 * M_PI;
 
-void    get_closest_distance(
-    t_game *game, t_coord horizontal, t_coord vertical)
-{
-    t_coord p_pos;
-    double  horizontal_dist;
-    double  vertical_dist;
+//     slope = tan(ray_angle);
+    
+//     if (ray_angle > 0 && ray_angle < M_PI)
+//     {
+//         intercept.intercept.y = floor(p_pos.y / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
+//         intercept.inter_dir = South;
+//         step.y = TILE_SIZE;
+//     }
+//     else
+//     {
+//         intercept.intercept.y = floor(p_pos.y / TILE_SIZE) * TILE_SIZE - EPSILON;
+//         intercept.inter_dir = North;
+//         step.y = -TILE_SIZE;
+//     }
+    
+//     if (fabs(slope) > 1e-10)
+//     {
+//         intercept.intercept.x = p_pos.x + (intercept.intercept.y - p_pos.y) / slope;
+//         step.x = step.y / slope;
+//     }
+//     else
+//     {
+//         intercept.intercept.x = p_pos.x;
+//         step.x = 0;
+//     }
 
-    p_pos = game->player.p_pos;
+//     return (check_intersection_hit(game, intercept, step));
+// }
 
-    horizontal_dist = get_distance(p_pos, horizontal);
-    vertical_dist = get_distance(p_pos, vertical);
-    if (horizontal_dist < vertical_dist)
-        game->cast_data.horizontal_hit = true;
-    else
-        game->cast_data.horizontal_hit = false;
-}
+// t_intercept_hit get_vertical_intersection(t_game *game, t_coord p_pos, double ray_angle)
+// {
+//     double          slope;
+//     t_intercept_hit intercept;
+//     t_coord         step;
 
+//     while (ray_angle < 0) ray_angle += 2 * M_PI;
+//     while (ray_angle >= 2 * M_PI) ray_angle -= 2 * M_PI;
 
-int     is_hit(t_game *game, t_coord intercept)
-{
-    int map_x = (int)(intercept.x / TILE_SIZE);
-    int map_y = (int)(intercept.y / TILE_SIZE);
+//     slope = tan(ray_angle);
+    
+//     if (ray_angle < M_PI / 2 || ray_angle > (3 * M_PI) / 2)
+//     {
+//         intercept.intercept.x = floor(p_pos.x / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
+//         intercept.inter_dir = East;
+//         step.x = TILE_SIZE;
+//     }
+//     else
+//     {
+//         intercept.intercept.x = floor(p_pos.x / TILE_SIZE) * TILE_SIZE - EPSILON;
+//         intercept.inter_dir = West;
+//         step.x = -TILE_SIZE;
+//     }
+    
+//     intercept.intercept.y = p_pos.y + (intercept.intercept.x - p_pos.x) * slope;
+//     step.y = step.x * slope;
 
-    if (map_x < 0 || map_x >= game->width ||
-        map_y < 0 || map_y >= game->map_height)
-        return (1);
-    return (game->map[map_y][map_x] == '1');
-}
+//     return (check_intersection_hit(game, intercept, step));
+// }
 
-t_intercept_hit	check_intersection_hit(
-    t_game *game, t_intercept_hit *intercept, t_coord step)
-{
-    while (true)
-    {
-        if (is_hit(game, intercept->intercept))
-            break;
-        intercept->intercept.x += step.x;
-        intercept->intercept.y += step.y;
-    }
-    return (*intercept);
-}
+// int is_hit(t_game *game, t_coord intercept)
+// {
+//     int map_x;
+//     int map_y;
+    
+//     map_x = (int)(intercept.x / TILE_SIZE);
+//     map_y = (int)(intercept.y / TILE_SIZE);
 
-// ----------- horizontal intersection -----------
-t_intercept_hit	get_horizontal_intersection(
-    t_game *game, t_coord p_pos, double ray_angle)
-{
-    double          slope;
-    t_intercept_hit intercept;
-    t_coord         step;
+//     if (map_x < 0 || map_x >= game->cast_data.map_x ||
+//         map_y < 0 || map_y >= game->cast_data.map_y)
+//         return (1);
+    
+//     return (game->map[map_y][map_x] == '1');
+// }
 
-    slope = tan(ray_angle);
-    if (ray_angle > 0 && ray_angle < M_PI)
-    {
-        intercept.intercept.y = floor(
-            p_pos.y / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
-        intercept.inter_dir = South;
-        step.y = TILE_SIZE;
-    }
-    else
-    {
-		intercept.intercept.y = floor(
-            p_pos.y / TILE_SIZE) * TILE_SIZE - 0.0001;
-        intercept.inter_dir = North;
-        step.y = -TILE_SIZE;
-    }
-    intercept.intercept.x = p_pos.x + (
-        intercept.intercept.y - p_pos.y) / slope;
-    step.x = step.y / slope;
-	return (check_intersection_hit(game, &intercept, step));
-}
-
-// ----------- vertical intersection -----------
-t_intercept_hit get_vertical_intersection(
-    t_game *game, t_coord p_pos, double ray_angle)
-{
-    double          slope;
-    t_intercept_hit intercept;
-    t_coord         step;
-
-    slope = tan(ray_angle);
-    if (ray_angle < M_PI / 2 || ray_angle > (M_PI * 3) / 2)
-    {
-        intercept.intercept.x = floor(
-            p_pos.x / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
-        intercept.inter_dir = East;
-        step.x = TILE_SIZE;
-    }
-    else
-    {
-        intercept.intercept.x = floor(
-            p_pos.x / TILE_SIZE) * TILE_SIZE - 0.0001;
-        intercept.inter_dir = West;
-        step.x = -TILE_SIZE;
-    }
-    intercept.intercept.y = p_pos.y + (
-        intercept.intercept.x - p_pos.x) * slope;
-    step.y = step.x * slope;
-    return (check_intersection_hit(game, &intercept, step));
-}
-
-t_intercept_hit cast_ray(t_game *game, double ray_angle)
-{
-    t_intercept_hit	horizontal_hit;
-	t_intercept_hit vertical_hit;
-
-    horizontal_hit = get_horizontal_intersection(
-        game, game->player.p_pos, ray_angle);
-    vertical_hit = get_vertical_intersection(
-        game, game->player.p_pos, ray_angle);
-    get_closest_distance(
-        game, horizontal_hit.intercept, vertical_hit.intercept);
-    if (game->cast_data.horizontal_hit)
-        return (horizontal_hit);
-    return (vertical_hit);
-}
-
-// ---------------------------------------------------------
-// ---------------------------------------------------------
-// ---------------------------------------------------------
-void	x_draw_line(t_game *game, double x0, double y0, double x1, double y1, int color)
-{
-	double	dx = x1 - x0;
-	double	dy = y1 - y0;
-	int		steps = (fabs(dx) > fabs(dy)) ? fabs(dx) : fabs(dy);
-	double	x_inc = dx / steps;
-	double	y_inc = dy / steps;
-	double	x = x0;
-	double	y = y0;
-	int		i = 0;
-
-	while (i <= steps)
-	{
-		my_mlx_pixel_put(game, (int)x, (int)y, color);
-		x += x_inc;
-		y += y_inc;
-		i++;
-	}
-}
-// --------------------------------------------------------------
-// --------------------------------------------------------------
-// --------------------------------------------------------------
-
-void	draw_front_view(t_game *game)
-{
-	int				column;
-	double			ray_angle;
-	t_coord			p_pos;
-	t_intercept_hit	ray_hit;
-
-	column = 0;
-	p_pos = game->player.p_pos;
-	ray_angle = game->player.angle - (game->player.fov / 2);
-	while (column < game->cast_data.ray_nbr)
-	{
-		ray_hit = cast_ray(game, ray_angle);
-
-		// >>> this like just an test of the ray is cast correctly before the 3d set
-		x_draw_line(game, p_pos.x, p_pos.y, ray_hit.intercept.x, ray_hit.intercept.y, GREEN_CLOOR);
-
-		ray_angle += game->cast_data.angle_step;
-		column++;
-	}
-}
-
-// >>> main game randring engine start here
-int	game_loop(t_game *game)
-{
-	handle_key_press(game);		// key-hadling
-
-	// 2D-top-view
-	draw_2d_map(game);
-	draw_player(game);
-
-	draw_front_view(game);		// >>> currently working here <<<
-	
-	mlx_put_image_to_window(game->window.mlx_ptr, 
-		game->window.win_ptr, 
-		game->window.main_img.img_ptr, 0, 0);
-
-	return (0);
-}
+// t_intercept_hit check_intersection_hit(t_game *game, t_intercept_hit intercept, t_coord step)
+// {
+//     while (true)
+//     {
+//         if (is_hit(game, intercept.intercept))
+//             break;
+            
+//         intercept.intercept.x += step.x;
+//         intercept.intercept.y += step.y;
+//         iterations++;
+//     }
+    
+//     return (intercept);
+// }

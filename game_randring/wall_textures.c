c440abe5b9e6aed80ba9a44813f8cd72e39cc8be
Youness Anflous <anayouness51@gmail.com>
2025-08-06 15:43:37 +0100

    just an update of the struct

--- game_randring/wall_textures.c
+++ game_randring/wall_textures.c
@@ -6,7 +6,7 @@
 /*   By: yanflous <yanflous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 10:58:00 by yanflous          #+#    #+#             */
-/*   Updated: 2025/08/02 13:34:54 by yanflous         ###   ########.fr       */
+/*   Updated: 2025/08/06 15:42:21 by yanflous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,15 @@ double	get_tex_slice_hit(t_game *game, t_intercept_hit obj_hit)
 double	get_texture_offset_y(int wall_height, double tex_step_per_pixel)
 {
 	int		wall_top_offset;
-	double	tex_pos;
+	double	tex_index;
 
-	tex_pos = 0;
+	tex_index = 0;
 	wall_top_offset = (SCREEN_HEIGHT / 2) - (wall_height / 2);
 	if (wall_top_offset < 0)
-		tex_pos = (-wall_top_offset) * tex_step_per_pixel;
+		tex_index = (-wall_top_offset) * tex_step_per_pixel;
 	else
-		tex_pos = 0;
-	return (tex_pos);
+		tex_index = 0;
+	return (tex_index);
 }
 
 void	set_wall_textures(t_game *game, int row_slice, int ceiling, int floor)
@@ -56,12 +56,12 @@ void	set_wall_textures(t_game *game, int row_slice, int ceiling, int floor)
 	t_image		*img;
 	double		tex_step_per_pixel;
 	double		tex_slice_hit;
-	double		tex_pos;
+	double		tex_index;
 	int			wall_top_offset;
 
 	img = get_img_ptr(game, game->cast_data.obj_hit);
 	tex_step_per_pixel = (double)img->height / game->cast_data.wall_height;
-	tex_pos = get_texture_offset_y(
+	tex_index = get_texture_offset_y(
 			game->cast_data.wall_height, tex_step_per_pixel);
 	tex_slice_hit = get_tex_slice_hit(
 			game, game->cast_data.obj_hit);
@@ -70,12 +70,12 @@ void	set_wall_textures(t_game *game, int row_slice, int ceiling, int floor)
 	wall_top_offset = (ceiling - 1);
 	while (++wall_top_offset < floor)
 	{
-		game->cast_data.tex_offset_y = (int)tex_pos % img->height;
+		game->cast_data.tex_offset_y = (int)tex_index % img->height;
 		game->cast_data.color = *(int *)(img->addr
 				+ game->cast_data.tex_offset_y * img->size_line
 				+ game->cast_data.tex_offset_x * (img->bpp / 8));
 		my_mlx_pixel_put(
 			game, row_slice, wall_top_offset, game->cast_data.color);
-		tex_pos += tex_step_per_pixel;
+		tex_index += tex_step_per_pixel;
 	}
 }
